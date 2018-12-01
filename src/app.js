5dcf962d41e098ae84a3c9c5819eae41118f445d
bodiddlie <bodiddlie@gmail.com>
2018-12-01 12:05:03 -0700

    added better examples for lazy
    added back about and contact as smaller/simpler examples of lazy
    added stupid animation for loading

--- src/app.js
+++ src/app.js
@@ -2,25 +2,41 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 import Home from './home';
-const NonMemo = lazy(() => import('./nonmemo'));
-const Contact = lazy(() => import('./memo'));
+import NonMemo from './nonmemo';
+import Memo from './memo';
+const About = lazy(() => import('./about'));
+const Contact = lazy(() => import('./contact'));
 
 export default class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <h1>App</h1>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
           <Link to="/">Home</Link>
-          <Link to="/nonmemo/">About</Link>
-          <Link to="/memo/">Contact Us</Link>
-          <Suspense fallback={<div>Loading......</div>}>
-            <Route path="/" exact component={Home} />
-            <Route path="/nonmemo/" render={props => <NonMemo {...props} />} />
-            <Route path="/memo/" render={props => <Contact {...props} />} />
-          </Suspense>
+          <Link to="/about/">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/nonmemo/">Non Memoized</Link>
+          <Link to="/memo/">Memoized</Link>
+          <div>
+            <Suspense fallback={<Loading />}>
+              <Route path="/" exact component={Home} />
+              <Route path="/about/" render={props => <About {...props} />} />
+              <Route
+                path="/contact/"
+                render={props => <Contact {...props} />}
+              />
+              <Route path="/nonmemo/" component={NonMemo} />
+              <Route path="/memo/" component={Memo} />
+            </Suspense>
+          </div>
         </div>
       </BrowserRouter>
     );
   }
 }
+
+const Loading = () => (
+  <span className="loading" role="img" aria-label="Derp">
+    🤪
+  </span>
+);
